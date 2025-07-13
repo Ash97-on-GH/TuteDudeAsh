c48f7f8468d38928afbccb01fe181c3a41c65381
Ashutosh <ashung9197@gmail.com>
2025-07-13 23:33:22 +0530

    Add Assignment 2 task 2 script

---
+++ Assignment 2 Task 2.py
@@ -0,0 +1,11 @@
+'''
+Task 2: Write a Python program that:
+1.   Uses a for loop to iterate over numbers from 1 to 50.
+2.   Calculates the sum of all integers in this range.
+3.   Displays the final sum.
+
+'''
+S = 0
+for i in range(1,51):
+    S += i
+print("The sum of all integers from 1 to 50 is: ",S)
